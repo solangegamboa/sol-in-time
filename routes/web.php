8a458e3b6e8830cbc8a4284ef0802a62be725b7e
Sol Gamboa <solange.gamboa@dentalspeed.com>
2021-07-07 21:10:58 -0300

    Página padrão para login / Timezone BR

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('home');;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
